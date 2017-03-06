5415676502e017e48271d582d2fad9b41f4323e3
Rachel Eiting <reiting1@gmail.com>
2017-03-06 16:29:42 -0600

    initial commit

---
+++ server/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var path = require('path');
+var bodyparser = require('body-parser');
+var app = express();
+
+app.get('/', function(req, res){
+
+});
+app.listen(3000);
