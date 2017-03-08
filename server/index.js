f71cd79e614f10efd035b92ced032a818598e294
Rachel Eiting <reiting1@gmail.com>
2017-03-08 09:38:22 -0600

    removed unncessary info and added comments to understand what each part does

--- server/index.js
+++ server/index.js
@@ -4,36 +4,35 @@ var bodyParser = require('body-parser');
 var fs = require('fs');
 var app = express();
 
+//path.join is like a cursor. dirname gives the absolute path to the folder we're in. 
 var clientPath = path.join(__dirname, "../client");
-var jsonPath = path.join(__dirname + "/data.json");
+var jsonPath = path.join(__dirname, "data.json");
 
-
-app.use(express.static("client"));
+//express.static is a function 
+app.use(express.static(clientPath));
 app.use(bodyParser.json());
-
-
-app.get("/", function (req, res) {
-    res.send(clientPath + "index.html");
-});
-
 
 app.route('/api/chirps')
     .get(function (req, res) {
         res.sendFile(jsonPath);
     })
     .post(function (req, res) {
-       
+        //utf8 means I get a string, not a buffer
             fs.readFile(jsonPath, "utf8", function (err, data) {
                 if (err) {
-                    console.log("you have an error");
-                    res.sendStatus(404);
+                    //500 means internal server error
+                    res.sendStatus(500);
                 } else {
+                    //creates javascript array
                     var allChirps = JSON.parse(data);
-
-                    allChirps.push(req.body);
+                    //info in new chirp
+                    var newChirp = req.body;
+                    //pushed new chirp onto the array
+                    allChirps.push(newChirp);
+                    //tell it to go to the json file, make the array a json object, and callback for error
                     fs.writeFile(jsonPath, JSON.stringify(allChirps), function (err) {
                         if (err) {
-                            res.sendStatus(404);
+                            res.sendStatus(500);
                         } else {
                             res.sendStatus(201);
                         }
@@ -42,9 +41,5 @@ app.route('/api/chirps')
         });
     });
 
-
-
-//read the file, parse the json array, push the chirp onto the array, write the stringified array to the file 
-
-
 app.listen(3000);
+console.log("server listening on port 3000");
